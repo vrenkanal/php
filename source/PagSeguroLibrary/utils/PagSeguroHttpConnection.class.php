fd014c1f5a29390ce210c35972771a68e66cfbaa
brunolanaros2it <bruno.lanaro@s2it.com.br>
2013-10-07 12:49:25 -0300

    Correções
    Correções apontadas pelo Travis

--- source/PagSeguroLibrary/utils/PagSeguroHttpConnection.class.php
+++ source/PagSeguroLibrary/utils/PagSeguroHttpConnection.class.php
@@ -107,8 +107,8 @@ class PagSeguroHttpConnection
         
         // adding PHP version
         if (!is_null(PagSeguroLibrary::getPHPVersion())) {
-            array_push($options[CURLOPT_HTTPHEADER], 'language-engine-description: '
-                    . PagSeguroLibrary::getPHPVersion());
+            array_push($options[CURLOPT_HTTPHEADER],
+			        'language-engine-description: ' . PagSeguroLibrary::getPHPVersion());
         }
 
         $options = ($options + $methodOptions);

22b650eee53706df6b38a9953ba49054083171ab
cegoncalves <cegoncalves@uolinc.com>
2014-11-19 17:58:31 -0200

    Identacao do exemplo de criacao de transacao com checkout lightbox

--- source/examples/createPaymentRequestLightbox.php
+++ source/examples/createPaymentRequestLightbox.php
@@ -9,13 +9,17 @@
     </head>
 </html>
 <?php
+
 /*
  * ***********************************************************************
  Copyright [2011] [PagSeguro Internet Ltda.]
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
+
  http://www.apache.org/licenses/LICENSE-2.0
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,25 +27,33 @@
  limitations under the License.
  * ***********************************************************************
  */
+
 require_once "../PagSeguroLibrary/PagSeguroLibrary.php";
+
 /**
  * Class with a main method to illustrate the usage of the domain class PagSeguroPaymentRequest
  */
 class CreatePaymentRequestLightbox
 {
+
     public static function main()
     {
         // Instantiate a new payment request
         $paymentRequest = new PagSeguroPaymentRequest();
+
         // Set the currency
         $paymentRequest->setCurrency("BRL");
+
         // Add an item for this payment request
         $paymentRequest->addItem('0001', 'Notebook prata', 2, 430.00);
+
         // Add another item for this payment request
         $paymentRequest->addItem('0002', 'Notebook rosa', 2, 560.00);
+
         // Set a reference code for this payment request, it is useful to identify this payment
         // in future notifications.
         $paymentRequest->setReference("REF123");
+
         // Set shipping information for this payment request
         $sedexCode = PagSeguroShippingType::getCodeByType('SEDEX');
         $paymentRequest->setShippingType($sedexCode);
@@ -55,6 +67,7 @@ class CreatePaymentRequestLightbox
             'SP',
             'BRA'
         );
+
         // Set your customer information.
         $paymentRequest->setSender(
             'João Comprador',
@@ -64,12 +77,15 @@ class CreatePaymentRequestLightbox
             'CPF',
             '156.009.442-76'
         );
+
         // Set the url used by PagSeguro to redirect user after checkout process ends
         $paymentRequest->setRedirectUrl("http://www.lojamodelo.com.br");
+
         // Add checkout metadata information
         $paymentRequest->addMetadata('PASSENGER_CPF', '15600944276', 1);
         $paymentRequest->addMetadata('GAME_NAME', 'DOTA');
         $paymentRequest->addMetadata('PASSENGER_PASSPORT', '23456', 1);
+
         // Another way to set checkout parameters
         $paymentRequest->addParameter('notificationURL', 'http://www.lojamodelo.com.br/nas');
         $paymentRequest->addParameter('senderBornDate', '07/05/1981');
@@ -77,36 +93,38 @@ class CreatePaymentRequestLightbox
         $paymentRequest->addIndexedParameter('itemDescription', 'Notebook Preto', 3);
         $paymentRequest->addIndexedParameter('itemQuantity', '1', 3);
         $paymentRequest->addIndexedParameter('itemAmount', '200.00', 3);
+
         try {
+
             /*
              * #### Credentials #####
              * Replace the parameters below with your credentials (e-mail and token)
              * You can also get your credentials from a config file. See an example:
              * $credentials = PagSeguroConfig::getAccountCredentials();
              */
-           $credentials = new PagSeguroAccountCredentials("vendedor@lojamodelo.com.br",
+            $credentials = new PagSeguroAccountCredentials("vendedor@lojamodelo.com.br",
                "E231B2C9BCC8474DA2E260B6C8CF60D3");
-
             // Register this payment request in PagSeguro to obtain the checkout code
             $onlyCheckoutCode = true;
             $code = $paymentRequest->register($credentials, $onlyCheckoutCode);
-            
-            self::printPaymentUrl($code);
 
+            self::printPaymentUrl($code);
         } catch (PagSeguroServiceException $e) {
             die($e->getMessage());
         }
     }
+
     public static function printPaymentUrl($code)
     {
         if ($code) {
             echo "<h2>Criando requisi&ccedil;&atilde;o de pagamento</h2>";
             echo "<p>Code: <strong>$code</strong></p>";
             echo "<script>
-            PagSeguroLightbox('".$code."');         
+            PagSeguroLightbox('".$code."');
                   </script>";
-    
+
         }
     }
 }
+
 CreatePaymentRequestLightbox::main();

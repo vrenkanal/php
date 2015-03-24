0cc414e581d61badc6a984b42edbae6826c1eee0
Edison Silva Jr <edison.junior@s2it.com.br>
2015-03-24 14:48:08 -0300

    Fix: GetAccountCredentials.

--- source/examples/createPaymentRequest.php
+++ source/examples/createPaymentRequest.php
@@ -122,12 +122,13 @@ class CreatePaymentRequest
              * #### Credentials #####
              * Replace the parameters below with your credentials
              * You can also get your credentials from a config file. See an example:
-             * $credentials = PagSeguroConfig::getAccountCredentials();
-            //  */
+             * $credentials = new PagSeguroAccountCredentials("vendedor@lojamodelo.com.br",
+             * "E231B2C9BCC8474DA2E260B6C8CF60D3");
+             */
 
             // seller authentication
-            $credentials = new PagSeguroAccountCredentials("vendedor@lojamodelo.com.br",
-                "E231B2C9BCC8474DA2E260B6C8CF60D3");
+            $credentials = PagSeguroConfig::getAccountCredentials();
+            
 
             // application authentication
             //$credentials = PagSeguroConfig::getApplicationCredentials();
